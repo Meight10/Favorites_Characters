dea6a5005e91d6f70682c996e0f1c1c98ecae10e
Meight <7ace.lex@gmail.com>
2018-04-25 09:16:17 -0600

    eliminando MainActivvity generada automaticamente por tabbedActivity para hacerla que funcione con la implementacion del recylerview

--- app/src/main/java/com/example/uca/favoritescharacters/MainActivity.java
+++ app/src/main/java/com/example/uca/favoritescharacters/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.uca.favoritescharacters;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -20,135 +22,107 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-
-    ArrayList<Personajes> charactersList;
-    RecyclerView recyclerViewPersonajes;
-    private SectionsPagerAdapter mSectionsPagerAdapter;
+public class MainActivity extends AppCompatActivity implements List_Fragment.OnListFragmentInteractionListener{
 
 
-    private ViewPager mViewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        //define TabLayout
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_text_1));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_text_2));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        //define viewPager
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.container);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        //fill the list
+        charactersList = createCharactersListModel();
+        createCharactersListModel();
 
-        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //adding a adapter to viewPager
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount(), charactersList);
+
+        viewPager.setAdapter(adapter);
+
+        //Listener
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(getOnTabSelectedListener(viewPager));
+
+
+
+
+
+
+    }
+
+    /**
+     * Listener for tab selected
+     * @param viewPager
+     * @return
+     */
+
+    private TabLayout.OnTabSelectedListener getOnTabSelectedListener(final ViewPager viewPager) {
+        return new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                Toast.makeText(MainActivity.this, "Tab selected " +  tab.getPosition(), Toast.LENGTH_SHORT).show();
             }
-        });
 
-        charactersList = new ArrayList<>();
-        recyclerViewPersonajes = findViewById(R.id.recyclerview);
-        recyclerViewPersonajes.setLayoutManager(new LinearLayoutManager(this));
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                // nothing now
+            }
 
-        fillCharcters();
-
-        PersonajesAdapter adapter = new PersonajesAdapter(charactersList);
-        recyclerViewPersonajes.setAdapter(adapter);
-
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                // nothing now
+            }
+        };
     }
-
-    private void fillCharcters() {
-        charactersList.add(new Personajes("Sombra", "", R.drawable.sombra));
-        charactersList.add(new Personajes("Bastion", "", R.drawable.bastion));
-        charactersList.add(new Personajes("Genji", "", R.drawable.genji));
-        charactersList.add(new Personajes("Hanzo", "", R.drawable.hanzo));
-        charactersList.add(new Personajes("Junkrat", "", R.drawable.junkrat));
-        charactersList.add(new Personajes("Mccree", "", R.drawable.mccree));
-        charactersList.add(new Personajes("Mei", "", R.drawable.mei));
-        charactersList.add(new Personajes("Pharah", "", R.drawable.pharah));
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
 
     /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
+     * Listener that comunicate fragment / recycler with this activity
+     * @param model
      */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-
-            switch (position){
-                case 0:
-                    Tab1_List tab1 = new Tab1_List();
-                    return tab1;
-                case 1:
-                    Tab2_Favorites tab2 = new Tab2_Favorites();
-                    return tab2;
-                default:
-                    return null;
-            }
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 2;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position){
-            switch (position){
-                case 0:
-                    return "Champions";
-                case 1:
-                    return "Favorites";
-            }
-
-            return null;
-        }
+    @Override
+    public void onListFragmentInteraction(Personajes model) {
+        // the user clicked on this item over the list
+        Toast.makeText(MainActivity.this, Personajes.class.getSimpleName() + ":" + model.getName() + " - "  +model.getDescription(), Toast.LENGTH_LONG).show();
     }
+
+    // model for test purpose
+    private Personajes[] createCharactersListModel() {
+
+        List<Personajes> heroes =  new ArrayList<>();
+
+        heroes.add(new Personajes("Sombra", "", R.drawable.sombra));
+        heroes.add(new Personajes("Bastion", "", R.drawable.bastion));
+        heroes.add(new Personajes("Genji", "", R.drawable.genji));
+        heroes.add(new Personajes("Hanzo", "", R.drawable.hanzo));
+        heroes.add(new Personajes("Junkrat", "", R.drawable.junkrat));
+        heroes.add(new Personajes("Mccree", "", R.drawable.mccree));
+        heroes.add(new Personajes("Mei", "", R.drawable.mei));
+        heroes.add(new Personajes("Pharah", "", R.drawable.pharah));
+
+        return heroes.toArray(new Personajes[heroes.size()]);
+    }
+
+
+    Personajes[] charactersList;
+
 }
