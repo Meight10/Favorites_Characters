a731810166524d727cab34f182d7dfbb661b1b50
cs <00029613@uca.edu.sv>
2018-04-25 13:33:00 -0600

    agregando a la tab2, la lista modelo

--- app/src/main/java/com/example/uca/favoritescharacters/PagerAdapter.java
+++ app/src/main/java/com/example/uca/favoritescharacters/PagerAdapter.java
@@ -10,11 +10,13 @@ public class PagerAdapter extends FragmentStatePagerAdapter {
 
     private int numTabs;
     private Personajes[] heroesListt0;
+    private Personajes[] heroesListt1;
 
-    public PagerAdapter(FragmentManager fm, int numTabs, Personajes[] heroesListt0) {
+    public PagerAdapter(FragmentManager fm, int numTabs, Personajes[] heroesListt0, Personajes[] heroesListt1) {
         super(fm);
         this.numTabs = numTabs;
         this.heroesListt0 = heroesListt0;
+        this.heroesListt1 = heroesListt1;
     }
 
     @Override
@@ -24,7 +26,8 @@ public class PagerAdapter extends FragmentStatePagerAdapter {
                 List_Fragment tab1 = List_Fragment.newInstance(heroesListt0);
                 return tab1;
             case 1:
-                return null;
+                List_Fragment tab2 = List_Fragment.newInstance(heroesListt1);
+                return tab2;
             default:
                 throw new RuntimeException("Tab position invalid" + position);
         }
