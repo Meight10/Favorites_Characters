4fea9c54f69fe406c0398e94802ce2ba7773cbe6
Meight <7ace.lex@gmail.com>
2018-05-05 20:57:53 -0600

    clean up code

--- app/src/main/java/com/example/uca/favoritescharacters/adapters/PagerAdapter.java
+++ app/src/main/java/com/example/uca/favoritescharacters/adapters/PagerAdapter.java
@@ -1,12 +1,12 @@
-package com.example.uca.favoritescharacters;
+package com.example.uca.favoritescharacters.adapters;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.util.Log;
+
+import com.example.uca.favoritescharacters.model.Personajes;
 
 import java.util.ArrayList;
 import java.util.List;
