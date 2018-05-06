1a2021a1e5e33290deac663b56552b122e5e687f
Meight <7ace.lex@gmail.com>
2018-05-05 20:58:41 -0600

    implementando metodo abstracto de List_Fragment para que agregue a las listas segun corresponde

--- app/src/main/java/com/example/uca/favoritescharacters/processes/MainActivity.java
+++ app/src/main/java/com/example/uca/favoritescharacters/processes/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.uca.favoritescharacters;
+package com.example.uca.favoritescharacters.processes;
 
 import android.support.design.widget.TabLayout;
 
@@ -7,6 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import com.example.uca.favoritescharacters.adapters.PagerAdapter;
+import com.example.uca.favoritescharacters.model.Personajes;
+import com.example.uca.favoritescharacters.R;
+import com.example.uca.favoritescharacters.fragments.List_Fragment;
 
 import java.util.ArrayList;
 
